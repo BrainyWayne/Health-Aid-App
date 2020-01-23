9e66b8d6fc4293572370679fcafe15c0ce06a461
Francis Eshun <eshunfrancisboateng@gmail.com>
2020-01-23 14:53:00 +0000

    Top Segment Populated

--- lib/pages/homeSegments/topSegment.dart
+++ lib/pages/homeSegments/topSegment.dart
@@ -23,7 +23,7 @@ class _homeTopSegmentState extends State<homeTopSegment> {
                   )),
               Container(
                 decoration: BoxDecoration(
-                  color: Colors.blue.withOpacity(0.9),
+                  color: Color(0xffcc00008b),
                 ),
               ),
               Container(
@@ -32,7 +32,9 @@ class _homeTopSegmentState extends State<homeTopSegment> {
                   mainAxisSize: MainAxisSize.max,
                   children: <Widget>[
                     Icon(Icons.menu, color: Colors.white),
-                    SizedBox(width: 20,),
+                    SizedBox(
+                      width: 20,
+                    ),
                     Row(
                       mainAxisSize: MainAxisSize.max,
                       children: <Widget>[
@@ -41,14 +43,108 @@ class _homeTopSegmentState extends State<homeTopSegment> {
                               color: Colors.white,
                               fontSize: 20,
                             )),
-                        Text("Aid",
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold,
-                              fontSize: 20,
-                            )),
+                        Text(
+                          "Aid",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                          ),
+                        ),
                       ],
                     ),
+                  ],
+                ),
+              ),
+              Container(
+                margin:
+                    EdgeInsets.only(top: 100, left: 20, right: 20, bottom: 20),
+                child: Row(
+                  children: <Widget>[
+                    CircleAvatar(
+                      radius: 50,
+                      backgroundImage: AssetImage(
+                        "assets/heart.png",
+                      ),
+                    ),
+                    SizedBox(
+                      width: 15,
+                    ),
+                    Text(
+                      "Francis Eshun",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                margin:
+                    EdgeInsets.only(top: 230, left: 0, right: 20, bottom: 20),
+                child: Column(
+                  children: <Widget>[
+                    Row(
+                      children: <Widget>[
+                        Container(
+                          height: 3,
+                          width: 50,
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                          ),
+                        ),
+                        SizedBox(
+                          width: 7,
+                        ),
+                        Text(
+                          "Weather Update",
+                          style: TextStyle(color: Colors.white, fontSize: 15),
+                        ),
+                      ],
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(
+                          top: 20, left: 30, right: 20, bottom: 5),
+                      child: Row(
+                        children: <Widget>[
+                          Text("36",
+                              style:
+                                  TextStyle(color: Colors.white, fontSize: 40)),
+                          Text(
+                            "\u00b0",
+                            style: TextStyle(color: Colors.white, fontSize: 45),
+                          ),
+                          SizedBox(
+                            width: 20,
+                          ),
+                          Container(
+                            width: 50,
+                            height: 50,
+                            child: Image.asset(
+                              'assets/jotta-cloud.png',
+                            ),
+                          ),
+                          SizedBox(
+                            width: 16,
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: <Widget>[
+                              Text("Kumasi",
+                                  style: TextStyle(
+                                      color: Colors.white, fontSize: 18)),
+                              SizedBox(height: 3),
+                              Text("Mostly Sunny",
+                                  style: TextStyle(
+                                      color: Colors.white, fontSize: 15)),
+                            ],
+                          )
+                        ],
+                      ),
+                    )
                   ],
                 ),
               )
